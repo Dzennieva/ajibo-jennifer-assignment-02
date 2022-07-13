6ed50b4e8806a7f68d2b84f273a35dc98a9d04f2
Jennifer Ajibo <83118159+Dzennieva@users.noreply.github.com>
2022-07-13 22:54:03 +0000

    commented out challenge 3

--- src/assignment.js
+++ src/assignment.js
@@ -60,10 +60,10 @@ assignment.countEvenNumbers = countEvenNumbers;
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-  return arrayOfNumbers.map(number => number * (9/5) + 32); 
+  // return arrayOfNumbers.map(number => number * (9/5) + 32); 
   
 }
-assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
